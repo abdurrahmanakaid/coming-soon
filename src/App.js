77514e414bdf4af0d830334e23e1c8f1944edf62
abdurrahmanakaid <akaid.dev@gmail.com>
2022-01-09 13:04:10 -0800

    addd update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ const App = () => {
   return (
     <>
       <img src={logo} alt='logo' />
-      <h3>Coming Soon</h3>
-      <h2>We are building something awesome!!</h2>
-      <h1> tanvir constructions ltd </h1>
+      <h1>Coming Soon</h1>
+      <h3>We are building something awesome!!</h3>
     </>
   )
 }
