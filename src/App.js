cbe624f59d637e21e7a5410fe775d3e29e145845
abdurrahmanakaid <akaid.dev@gmail.com>
2022-01-09 12:49:43 -0800

    addd update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <img src={logo} alt='logo' />
+      <h3>Coming Soon</h3>
+      <h2>We are building something awesome!!</h2>
+      <h1> tanvir constructions ltd </h1>
+    </>
+  )
 }
 
-export default App;
+export default App
